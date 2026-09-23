4031d24d05a4e9473195842cc6f4f4b003c14436
Lony jock <lonyjock5555@gmail.com>
2026-09-23 14:03:53 +0300

    Add files via upload

---
+++ welcome.py
@@ -0,0 +1,11 @@
+
+# welcome.py
+
+def welcome(name):
+    return "Hello, " + name + "! Welcome to PLP."
+
+
+# Test the function
+print(welcome("Amina"))
+print(welcome("Brian"))
+print(welcome("Fatuma"))
